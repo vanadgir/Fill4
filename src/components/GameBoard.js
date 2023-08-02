6f93e5336227ca9ea73b785b81de966e2a441a68
Varun Nadgir <theminivann@gmail.com>
2023-08-02 08:54:44 -0400

    fixing dimensions

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -3,9 +3,7 @@ import ColoringGrid from "./ColoringGrid";
 export default function GameBoard() {
   return (
     <div className="game-board">
-      <div className="grid-space">
-        <ColoringGrid />
-      </div>
+      <ColoringGrid />
     </div>
   );
 }
